b6d192bfe431b94fd12fe7a899ce46b6ddc9056a
joaorodriguesjr <mail@joaorodriguesjr.com>
2021-07-30 09:31:46 -0300

    Draw an invader in the display

--- main.js
+++ main.js
@@ -13,29 +13,26 @@ canvas.height = ROWS * SCALE
 const display = new Display(COLS, ROWS)
 const renderer = new Renderer(canvas, SCALE)
 
-//display.pixelOn(0, 0)
-//display.pixelOn(COLS - 1, ROWS - 1)
-// renderer.render(display)
-
 
 const vm = new Machine(display, Config.instructions)
 
-vm.memory[0x000] = 0xF0
-vm.memory[0x001] = 0x90
-vm.memory[0x002] = 0xF0
-vm.memory[0x003] = 0x90
-vm.memory[0x004] = 0xF0
+vm.memory[0x000] = 0xBA
+vm.memory[0x001] = 0x7C
+vm.memory[0x002] = 0xD6
+vm.memory[0x003] = 0xFE
+vm.memory[0x004] = 0x54
+vm.memory[0x005] = 0xAA
 
 vm.V[1] = 5
 vm.V[2] = 5
 
-vm._DXYN(5, 1, 2)
+vm._DXYN(6, 1, 2)
 
 renderer.render(display)
 
 setInterval(() => {
 
-    //vm.cycle()
-    //renderer.render(display)
+    // vm.cycle()
+    // renderer.render(display)
 
 }, 1000 / 2)
