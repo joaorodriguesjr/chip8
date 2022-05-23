f1f6f13acdf8e99af864ccf4e2a190a1738b1243
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-05-23 16:40:54 -0300

    Remove ROMs list

--- public/main.js
+++ public/main.js
@@ -1,9 +1,19 @@
 import Chip8 from './src/Chip8.js'
 
 const chip8 = Chip8.create()
-document.querySelector('#power').onclick = () => {
-    const indicator = document.querySelector('.indicator')
+const indicator = document.querySelector('.indicator')
 
+function fetchRom(url) {
+    fetch(url).then(response => response.arrayBuffer()).then(buffer => {
+        chip8.machine.reset()
+        chip8.machine.load(buffer)
+        chip8.start()
+
+        indicator.classList.add('on')
+    })
+}
+
+document.querySelector('#power').onclick = () => {
     if (chip8.running)
         indicator.classList.remove('on')
     else
@@ -11,6 +21,7 @@ document.querySelector('#power').onclick = () => {
 
     chip8.power()
 }
+
 document.querySelector('#reset').onclick = () => chip8.reset()
 
 
@@ -24,88 +35,25 @@ window.onresize = (event) => {
     chip8.machine.display.render()
 }
 
-fetch(`/roms/Chip8 emulator Logo [Garstyciuks].ch8`)
-    .then(response => response.arrayBuffer())
-    .then(buffer => {
-        chip8.machine.reset()
-        chip8.machine.load(buffer)
-        chip8.start()
-        document.querySelector('.indicator').classList.add('on')
-    })
+fetchRom(`/roms/Chip8 emulator Logo [Garstyciuks].ch8`)
 
 fetch(`/roms/list.json`)
-    .then(response => response.json())
-    .then(roms => {
-        const parent = document.querySelector('#roms')
+    .then(response => response.json()).then(roms => {
+        let current = 0
 
-        let rom = 0
         document.querySelector('#prev').onclick = () => {
-            rom--
-            if (rom < 0)
-                rom = roms.length - 1
+            current--
+            if (current < 0)
+                current = roms.length - 1
 
-            fetch(`/roms/${roms[rom].file}`)
-            .then(response => response.arrayBuffer())
-            .then(buffer => {
-                chip8.machine.reset()
-                chip8.machine.load(buffer)
-                chip8.start()
-            })
-
+            fetchRom(`/roms/${roms[current].file}`)
         }
 
         document.querySelector('#next').onclick = () => {
-            rom++
-            if (rom >= roms.length)
-                rom = 0
+            current++
+            if (current >= roms.length)
+                current = 0
 
-            fetch(`/roms/${roms[rom].file}`)
-            .then(response => response.arrayBuffer())
-            .then(buffer => {
-                chip8.machine.reset()
-                chip8.machine.load(buffer)
-                chip8.start()
-            })
-
-        }
-
-
-        for (const rom of roms) {
-            const label = document.createElement('label')
-
-            const input = document.createElement('input')
-            input.type = 'radio'
-            input.name = 'rom'
-            input.value = rom.file
-            label.appendChild(input)
-
-            const span = document.createElement('span')
-            span.textContent = '- ' + rom.file
-            label.appendChild(span)
-
-            parent.appendChild(label)
-
-            if (rom.file === 'Chip8 emulator Logo [Garstyciuks].ch8') {
-                parent.scroll(0, label.offsetTop)
-                input.checked = true
-            }
-        }
-
-        const radios = document.querySelectorAll('input[name="rom"]')
-
-        for (const radio of radios) {
-            radio.onchange = (event) => {
-                chip8.stop()
-
-                fetch(`/roms/${event.target.value}`)
-                    .then(response => response.arrayBuffer())
-                    .then(buffer => {
-                        chip8.machine.reset()
-                        chip8.machine.load(buffer)
-                        chip8.start()
-                    })
-
-                return
-            }
+            fetchRom(`/roms/${roms[current].file}`)
         }
     })
